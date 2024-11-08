982d7547054a4850a97a644aa505df56b1043302
Liam Andersson <contactliamandersson@gmail.com>
2024-11-08 13:09:36 +0100

    Created input and used async/await

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,10 @@ import coin from './assets/coinFlip.gif';
 function App() {
   const [result, setResult] = useState<string>('');
   const [coinFlip, setCoinFlip] = useState<boolean>();
+  const [input, setInput] = useState<string>('');
 
 
-  fetch('https://api.adviceslip.com/advice')
-  .then((response) => response.json())
-  .then((data) => {
-    setResult(data.slip.advice);
-  })
-  .catch((error) => {
-    console.error('Error fetching advice: ', error)
-  });
-
-const flipCoin = (e: React.FormEvent) => {
-  e.preventDefault();
-  setCoinFlip(true);
-  setTimeout(() => {
+const flipCoin = () => {
     return new Promise((resolve, reject) => {
       let coinResult = Math.random();
       if (coinResult > 0.5) {
@@ -27,11 +16,26 @@ const flipCoin = (e: React.FormEvent) => {
       } else {
         reject('You Lose!')
       }
-        setCoinFlip(false);
     });
-  }, 1500);
-  
 };
+
+const getAdvice = async (e: React.FormEvent) => {
+  e.preventDefault();
+  setCoinFlip(true);
+  setTimeout(async () => {
+    try {
+      const coinFlipResult = await flipCoin();
+      console.log(coinFlipResult)
+      const response = await fetch(`https://api.adviceslip.com/advice/${input}`)
+      const data = await response.json();
+      setResult(data.slip.advice)
+    } catch (error) {
+      console.error('Error fetching advice', error)
+    } finally {
+      setCoinFlip(false);
+    }
+  }, 1500);
+}
 
 
   return (
@@ -41,7 +45,16 @@ const flipCoin = (e: React.FormEvent) => {
         <img src={coin} className= 'img' alt="Coin Flipping" />
       </div>}
       <h1>Coin Flip and Advice</h1>
-      <button onClick={flipCoin}>Flip Coin</button>
+      <form onSubmit={getAdvice}>
+      <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Enter a number 1 - 224"
+              required
+            />
+      <button type="submit"> Flip Coin</button>
+      </form>
       <h3>{result}</h3>
     </>
   );
